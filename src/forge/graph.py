b5b02230cb61a1a7e3a6d8d0a19f1c7560ce1719
Arush <arush.devu@gmail.com>
2026-09-07 17:27:26 -0500

    Implement dependency graph resolution

--- src/forge/graph.py
+++ src/forge/graph.py
@@ -4,3 +4,15 @@ class DependencyGraph:
 
     def resolve(self):
         order = []
+        finished = set()
+        
+        while len(order) < len(self.tasks):
+            for task in self.tasks:
+                if task.name in finished:
+                    continue
+
+                if all(dependency in finished for dependency in task.dependencies):
+                    order.append(task.name)
+                    finished.add(task.name)
+
+        return order
